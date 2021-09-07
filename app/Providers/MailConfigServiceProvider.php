868dc5323429d599f6f8b68bed81d6b02fd9081f
Charles Shahi Thakuri <cst.programmer@gmail.com>
2021-09-07 23:16:27 +0545

    user & login setting function update

--- app/Providers/MailConfigServiceProvider.php
+++ app/Providers/MailConfigServiceProvider.php
@@ -24,6 +24,21 @@ class MailConfigServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+//        $mail_setting = systemSetting();
+//        if ($mail_setting) {
+//            $config = array(
+//                'driver'     => $mail_setting->mail_driver,
+//                'host'       => $mail_setting->mail_host_name,
+//                'port'       => $mail_setting->mail_port,
+//                'username'   => $mail_setting->mail_user_name,
+//                'password'   => $mail_setting->mail_password,
+//                'encryption' => $mail_setting->mail_encryption,
+//                'from'       => array('address' => $mail_setting->mail_from_address, 'name' => $mail_setting->app_name),
+//                'sendmail'   => '/usr/sbin/sendmail -bs',
+//                'pretend'    => false,
+//            );
+//
+//            Config::set('mail', $config);
+//        }
     }
 }
